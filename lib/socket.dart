db37d8b578c6ef5efc883b439ca0e8a3fd5fb42b
Tăng Vui <tangvuicntt@gmail.com>
2020-01-14 15:56:32 +0700

    fix arguments

--- lib/socket.dart
+++ lib/socket.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/services.dart';
 import 'dart:convert' show jsonDecode;
 import 'dart:async';
@@ -156,7 +158,8 @@ class SocketIO {
         try {
           return jsonDecode(_);
         } catch (e) {
-          //return _;
+          if(Platform.isIOS)
+            return _;
         }
       }).toList();
       arguments.removeWhere((value)=> value == null);
